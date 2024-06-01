7b7136e2a0b5da31efc5f06a0122682085c9edf0
MaryZ4E <20030296@itcelaya.edu.mx>
2024-05-31 23:20:31 -0600

    actualizacion del main cambie el appvaluenotifier por provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 import 'package:proyecto/screens/onboarding.dart';
-
 import 'package:proyecto/services/firebaseapi.dart';
-import 'package:proyecto/settings/app_value_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,57 +26,61 @@ void main() async {
         channelKey: 'key1',
         channelName: 'Basic notifications',
         channelDescription: 'Notification channel for basic tests',
-        defaultColor: Color(0xFF9D50DD),
+        defaultColor: const Color(0xFF9D50DD),
         ledColor: Colors.white,
       ),
     ],
   );
 
-  // Load theme color from Firestore
-  String userId = 'yourUserIdHere';
-  try {
-    Color? themeColor = await _loadThemeColorFromFirestore(userId);
-    if (themeColor != null) {
-      AppValueNotifier.setTheme(themeColor);
-    }
-  } catch (e) {
-    print('Error loading theme color: $e');
-    // Handle error loading theme color
-  }
-
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeColorProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
-Future<Color?> _loadThemeColorFromFirestore(String userId) async {
-  final userSnapshot =
-      await FirebaseFirestore.instance.collection('users').doc(userId).get();
-  if (userSnapshot.exists) {
-    final userData = userSnapshot.data() as Map<String, dynamic>;
-    return AppValueNotifier.getColorFromString(userData['color'] ?? 'blue');
-  } else {
-    throw Exception('User not found');
+class ThemeColorProvider with ChangeNotifier {
+  Color _themeColor = Colors.blue;
+
+  Color get themeColor => _themeColor;
+
+  Future<void> loadThemeColorFromFirestore(String userId) async {
+    final userSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    if (userSnapshot.exists) {
+      final userData = userSnapshot.data() as Map<String, dynamic>;
+      _themeColor =
+          getColorFromString(userData['color'] ?? 'blue') ?? Colors.blue;
+      notifyListeners();
+    } else {
+      throw Exception('User not found');
+    }
+  }
+
+  Color? getColorFromString(String colorString) {
+    return null;
+
+    // Implementar lógica para convertir una cadena en Color
+    // Retorna null si no se puede convertir
   }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: AppValueNotifier.themeNotifier,
-      builder: (context, ThemeData themeData, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: themeData,
-          home: Builder(
-            builder: (BuildContext context) {
-              final screenHeight = MediaQuery.of(context).size.height;
-              return Onboarding(screenHeight: screenHeight);
-            },
-          ),
-        );
-      },
+    final themeProvider = Provider.of<ThemeColorProvider>(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: themeProvider.themeColor),
+      home: Builder(
+        builder: (BuildContext context) {
+          final screenHeight = MediaQuery.of(context).size.height;
+          return Onboarding(screenHeight: screenHeight);
+        },
+      ),
     );
   }
 }
